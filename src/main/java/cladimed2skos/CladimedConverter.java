c018fb3453924c2e114cfaab8a4a7b41d89ee392
Elcio <elcioabrahao@usp.br>
2017-10-25 15:32:31 -0200

    New corrections
    -Join both schemas on the same file
    -Corretion of resource URIs on some tags
    -Add treatment for issue 2 (ABANDON)
    -Add namespaces on the file
    -Add ontology matadata on the file

--- src/main/java/cladimed2skos/CladimedConverter.java
+++ src/main/java/cladimed2skos/CladimedConverter.java
@@ -1,8 +1,10 @@
 package cladimed2skos;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.util.HashMap;
@@ -63,10 +65,20 @@ public class CladimedConverter {
 	public static final String MODIFIEDAT = "2016-12-31";
 	
 	
-	public static final String OWL_DEPRECATED = "<owl:deprecated>true</owl:deprecated>";
+	public static final String OWL_DEPRECATED = "<owl:deprecated rdf:resource=\"http://www.w3.org/2002/07/owl#deprecated\">true</owl:deprecated>";
 	
 	public static final String DATE_MODIFIED_OPEN = "<dcterms:modified rdf:datatype=\"http://www.w3.org/2001/XMLSchema#date\">";
 	public static final String DATE_MODIFIED_CLOSE = "</dcterms:modified>";
+	
+	public static final String SKOS_CHANGE_NOTE_OPEN = "<skos:changeNote rdf:resource=\"http://www.w3.org/2004/02/skos/core#changeNote\">";
+	
+	public static final String SKOS_CHANGE_NOTE_CLOSE = "</skos:changeNote>";
+	
+	public static final String PROV_INVALIDATED_AT_OPEN = "<prov:invalidatedAtTime rdf:resource=\"http://www.w3.org/ns/prov#invalidatedAtTime\">";
+	
+	public static final String PROV_INVALIDATED_AT_CLOSE = "</prov:invalidatedAtTime>";
+	
+	
 
 	
 	public CladimedConverter(Map<String,PrimaryEntry> simpleHash,Map<String,PrimaryEntry> completeHash) {
@@ -83,19 +95,15 @@ public class CladimedConverter {
 		
 		//Simple Classification
 		System.out.println();
-		System.out.println("Simple Classif Begin conversion...");
-		processSimpleClassification();
+		System.out.println("Begin conversion...");
+		processSingleDestinationFile();
 		System.out.println("Done...");
 		
-		//Complete Classification
-		System.out.println();
-		System.out.println("Complete Classif Begin conversion...");
-		processCompleteClassification();
-		System.out.println("Done...");
-		
+	
 		
 	}
-	
+
+	// deprecated in favor of processSingleDestinationFile()
 	public void processSimpleClassification(){
 		
 		PrimaryEntry item = null;
@@ -166,6 +174,7 @@ public class CladimedConverter {
 					line += SKOS_DCT_OPEN + "2016" + SKOS_DCT_CLOSE;
 					line += SKOS_PREF_LABEL_OPEN + item.getLabel().toLowerCase()+SKOS_PREF_LABEL_CLOSE;
 					line += SKOS_HIDDEN_LABEL_OPEN + item.getLabel().toUpperCase()+SKOS_HIDDEN_LABEL_CLOSE;
+					line += SKOS_CLASSIF_SIMPLE;
 					// variable part
 					if(item.getSf().isEmpty()){
 					line += SKOS_TOP_CONCEPT_OF_SIMPLE;	
@@ -191,7 +200,8 @@ public class CladimedConverter {
 		System.out.println("File sucessully generated: "+size+" lines");
 		
 	}
-	
+
+	// deprecated in favor of processSingleDestinationFile()
 	public void processCompleteClassification(){
 		
 		PrimaryEntry item = null;
@@ -322,6 +332,262 @@ public class CladimedConverter {
 	    } else {
 	    	return "";
 	    }
+	}
+	
+
+	private void processSingleDestinationFile(){
+		
+		PrimaryEntry item = null;
+
+		
+		// find broader and narrowers for single classification
+		
+		for (HashMap.Entry<String, PrimaryEntry> entry : simpleHash.entrySet()) {
+			item = entry.getValue();
+			//System.out.println(item.toString());
+			
+			if(item.getG().isEmpty()){
+				item.setBroaderConcept(item.getF());
+				if(simpleHash.get(item.getF())!=null){
+					simpleHash.get(item.getF()).addNarrowerConcepts(item.getCode());
+				}else{
+					item.addErrorDescription("No broader concept founded");
+				}
+			}else if(item.getSg().isEmpty()){
+				item.setBroaderConcept(item.getF()+item.getSf());
+				if(simpleHash.get(item.getF()+item.getSf())!=null){
+					simpleHash.get(item.getF()+item.getSf()).addNarrowerConcepts(item.getCode());
+				}else{
+					// Hotfix for concepts with no broader
+					item.setBroaderConcept(item.getF());
+					simpleHash.get(item.getF()).addNarrowerConcepts(item.getCode());
+				}
+			}else if(item.getNs().isEmpty()){
+				item.setBroaderConcept(item.getF()+item.getSf()+item.getG());
+				if(simpleHash.get(item.getF()+item.getSf()+item.getG())!=null){
+					simpleHash.get(item.getF()+item.getSf()+item.getG()).addNarrowerConcepts(item.getCode());
+				}else{
+					// Hotfix for concept without broader
+					item.setBroaderConcept(item.getF()+item.getSf());
+					if(simpleHash.get(item.getF()+item.getSf())!=null){
+						simpleHash.get(item.getF()+item.getSf()).addNarrowerConcepts(item.getCode());
+					}else{
+						item.setBroaderConcept(item.getF());
+						simpleHash.get(item.getF()).addNarrowerConcepts(item.getCode());
+					}				}
+			}else {
+				item.setBroaderConcept(item.getF()+item.getSf()+item.getG()+item.getSg());
+			}
+		}
+		
+		// find broader and narrowers for single classification
+
+		for (HashMap.Entry<String, PrimaryEntry> entry : completeHash.entrySet()) {
+			item = entry.getValue();
+			//System.out.println(item.toString());
+			
+			if(item.getG().isEmpty()){
+				item.setBroaderConcept(item.getF());
+				if(completeHash.get(item.getF())!=null){
+					completeHash.get(item.getF()).addNarrowerConcepts(item.getCode());
+				}else{
+					item.addErrorDescription("No broader concept founded");
+				}
+			}else if(item.getSg().isEmpty()){
+				item.setBroaderConcept(item.getF()+item.getSf());
+				if(completeHash.get(item.getF()+item.getSf())!=null){
+					completeHash.get(item.getF()+item.getSf()).addNarrowerConcepts(item.getCode());
+				}else{
+					// Hotfix for concept without broader
+					item.setBroaderConcept(item.getF());
+					completeHash.get(item.getF()).addNarrowerConcepts(item.getCode());
+				}
+			}else if(item.getNs().isEmpty()){
+				item.setBroaderConcept(item.getF()+item.getSf()+item.getG());
+				if(completeHash.get(item.getF()+item.getSf()+item.getG())!=null){
+					completeHash.get(item.getF()+item.getSf()+item.getG()).addNarrowerConcepts(item.getCode());
+				}else{
+					// Hotfix for concept without broader
+					item.setBroaderConcept(item.getF()+item.getSf());
+					if(completeHash.get(item.getF()+item.getSf())!=null){
+						completeHash.get(item.getF()+item.getSf()).addNarrowerConcepts(item.getCode());
+					}else{
+						item.setBroaderConcept(item.getF());
+						completeHash.get(item.getF()).addNarrowerConcepts(item.getCode());
+					}
+				}
+			}else {
+				item.setBroaderConcept(item.getF()+item.getSf()+item.getG()+item.getSg());
+			}
+		}
+		
+		
+		// Generate FILE
+		String filePath = destinationFolder+"/cladimed_classif_simple_and_complete_skos.txt";
+		String headerFilePath = destinationFolder+"/cladimed_header_metadata.txt";
+		System.out.println("File: "+ filePath);
+		File file = new File(filePath);
+		File headerFile = new File(headerFilePath);
+		String line="";
+		String sCurrentLine="";
+		int size=1;
+		
+	
+		
+		try{
+			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), "UTF8")); 
+			FileReader fr = new FileReader(headerFile);
+			BufferedReader br = new BufferedReader(fr);
+
+			// Adding header metadata
+			
+			while ((sCurrentLine = br.readLine()) != null) {
+				out.write(sCurrentLine); 
+		        out.newLine();          
+		        size++;
+			}			
+			
+			
+			// Generate SIMPLE SCHEMA
+			for (HashMap.Entry<String, PrimaryEntry> entry : simpleHash.entrySet()) {
+				item = entry.getValue();
+				if(item.getErrorDescription().isEmpty()){
+					line = SKOS_COMCEPT_OPEN_PRE + item.getCode()+SKOS_COMCEPT_OPEN_POS;
+					line += SKOS_NOTATION_OPEN + item.getCode()+ SKOS_NOTATION_CLOSE;
+					line += SKOS_DCT_OPEN + "2016" + SKOS_DCT_CLOSE;
+					line += SKOS_CLASSIF_SIMPLE;
+					// variable part
+					if(item.getSf().isEmpty()){
+					line += SKOS_TOP_CONCEPT_OF_SIMPLE;	
+					}else{
+						line += SKOS_BROADER_OPEN + item.getBroaderConcept()+SKOS_BROADER_CLOSE;
+					}
+					if(item.getNarrowerConcepts().size()>0){
+						for(String narrower: item.getNarrowerConcepts()){
+							line += SKOS_NARROWER_OPEN + narrower + SKOS_NARROWER_CLOSE;
+						}
+					}
+					line += SKOS_COMCEPT_CLOSE;
+					// Issue 2 for abandon on LABEL
+					if(item.getLabel().toUpperCase().indexOf("ABAN")>-1 && item.getObs1().isEmpty()){
+						line += OWL_DEPRECATED;
+						line += SKOS_CHANGE_NOTE_OPEN+item.getLabel()+SKOS_CHANGE_NOTE_CLOSE;
+						line += SKOS_PREF_LABEL_OPEN + item.getCode().toLowerCase()+SKOS_PREF_LABEL_CLOSE;
+						line += SKOS_HIDDEN_LABEL_OPEN + item.getCode().toUpperCase()+SKOS_HIDDEN_LABEL_CLOSE;
+
+					}else{
+						line += SKOS_PREF_LABEL_OPEN + item.getLabel().toLowerCase()+SKOS_PREF_LABEL_CLOSE;
+						line += SKOS_HIDDEN_LABEL_OPEN + item.getLabel().toUpperCase()+SKOS_HIDDEN_LABEL_CLOSE;
+					}
+					if(item.getLabel().toUpperCase().indexOf("ABAN")>-1 && !item.getObs1().isEmpty()){
+						if(item.getObs1().equalsIgnoreCase(ABANDON2016)){
+							line += PROV_INVALIDATED_AT_OPEN+MODIFIEDAT+PROV_INVALIDATED_AT_CLOSE;
+						}
+					}
+					// END issue 2
+					
+					
+					out.write(line); 
+			        out.newLine();          
+			        size++;
+					
+				}
+			}
+			
+			// Generate COMPLETE SCHEMA
+			
+			for (HashMap.Entry<String, PrimaryEntry> entry : completeHash.entrySet()) {
+				item = entry.getValue();
+				if(item.getErrorDescription().isEmpty()){
+					line = SKOS_COMCEPT_OPEN_PRE + item.getCode()+SKOS_COMCEPT_OPEN_POS;
+					line += SKOS_NOTATION_OPEN + item.getCode()+ SKOS_NOTATION_CLOSE;
+					line += SKOS_DCT_OPEN;
+					if(item.getObs1().equalsIgnoreCase(NEW2017)){
+						line += "2017";
+					}else{
+						line += "2016";
+					}
+					line +=SKOS_DCT_CLOSE;
+					// lines below commented due the issue 2 
+//					line += SKOS_PREF_LABEL_OPEN + item.getLabel().toLowerCase()+SKOS_PREF_LABEL_CLOSE;
+//					line += SKOS_HIDDEN_LABEL_OPEN + item.getLabel().toUpperCase()+SKOS_HIDDEN_LABEL_CLOSE;
+					line += SKOS_CLASSIF_COMPLETE;
+					if(simpleHash.get(item.getCode())!=null){
+						line += SKOS_CLASSIF_SIMPLE;
+					}
+					// variable part
+					if(item.getSf().isEmpty()){
+					line += SKOS_TOP_CONCEPT_OF_COMPLETE;	
+					}else{
+						line += SKOS_BROADER_OPEN + item.getBroaderConcept()+SKOS_BROADER_CLOSE;
+					}
+					if(item.getNarrowerConcepts().size()>0){
+						for(String narrower: item.getNarrowerConcepts()){
+							line += SKOS_NARROWER_OPEN + narrower + SKOS_NARROWER_CLOSE;
+						}
+					}
+					if(item.getObs1().equalsIgnoreCase(ABANDON2016)){
+						line += OWL_DEPRECATED;
+					}
+					if(item.getObs1().equalsIgnoreCase(MODIFIED2016)){
+						
+						line += DATE_MODIFIED_OPEN + MODIFIEDAT + DATE_MODIFIED_CLOSE;
+						line += SKOS_HIDDEN_LABEL_OPEN +item.getObs2().toUpperCase()+ SKOS_HIDDEN_LABEL_CLOSE;
+						
+					}
+					line += SKOS_COMCEPT_CLOSE;
+					// Issue 2 for abandon on LABEL
+					if(item.getLabel().toUpperCase().indexOf("ABAN")>-1 && item.getObs1().isEmpty()){
+						line += OWL_DEPRECATED;
+						line += SKOS_CHANGE_NOTE_OPEN+item.getLabel()+SKOS_CHANGE_NOTE_CLOSE;
+						line += SKOS_PREF_LABEL_OPEN + item.getCode().toLowerCase()+SKOS_PREF_LABEL_CLOSE;
+						line += SKOS_HIDDEN_LABEL_OPEN + item.getCode().toUpperCase()+SKOS_HIDDEN_LABEL_CLOSE;
+
+					}else{
+						line += SKOS_PREF_LABEL_OPEN + item.getLabel().toLowerCase()+SKOS_PREF_LABEL_CLOSE;
+						line += SKOS_HIDDEN_LABEL_OPEN + item.getLabel().toUpperCase()+SKOS_HIDDEN_LABEL_CLOSE;
+					}
+					if(item.getLabel().toUpperCase().indexOf("ABAN")>-1 && !item.getObs1().isEmpty()){
+						if(item.getObs1().equalsIgnoreCase(ABANDON2016)){
+							line += PROV_INVALIDATED_AT_OPEN+MODIFIEDAT+PROV_INVALIDATED_AT_CLOSE;
+						}
+					}
+					// END issue 2
+					
+					out.write(line); 
+			        out.newLine();          
+			        size++;
+					
+				}
+			}			
+			
+			
+		out.close();
+		}catch(IOException e){
+			e.printStackTrace();
+		}		  
+		System.out.println("File sucessully generated: "+size+" lines");		
+		
+		
+
+		// tags with "ABAND"
+//		for (HashMap.Entry<String, PrimaryEntry> entry : simpleHash.entrySet()) {
+//			item = entry.getValue();
+//			if(item.getLabel().toUpperCase().indexOf("ABAN")>-1){
+//				System.out.println("Simple   Concept: "+item.getCode()+" Label: "+item.getLabel()+" Message: "+item.getObs1()+" Obs: "+item.getObs2() );
+//			}
+//		}		
+//		
+//		// tags with "ABAND"
+//		for (HashMap.Entry<String, PrimaryEntry> entry : completeHash.entrySet()) {
+//			item = entry.getValue();
+//			if(item.getLabel().toUpperCase().indexOf("ABAN")>-1){
+//				System.out.println("Complete Concept: "+item.getCode()+" Label: "+item.getLabel()+" Message: "+item.getObs1()+" Obs: "+item.getObs2());
+//			}
+//		}		
+		
+		
+		
 	}
 	
 }
